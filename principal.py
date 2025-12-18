08f0f9bf26bd60cff367f18c1ed50decfcf38c5c
JAV13RG <91922179+JAV13RG@users.noreply.github.com>
2025-12-18 03:54:18 -0300

    Inicial Commit

---
+++ principal.py
@@ -0,0 +1,40 @@
+from negocio import registrar_usuario, iniciar_sesion
+from interfaces_usuario import menu_inicial
+from auxiliares import validar_correo
+from servicios.serper import busqueda
+# obtener_data_usuarios_api(url_usuarios)
+# listado_usuarios_db()
+# crear_user_api(url_usuarios)
+# eliminar_user_api(url_usuarios)
+# obtener_data_publicaciones(url_publicaciones)
+# listado_publicaciones()
+# registrar_usuario()
+
+
+def app():
+    while True:
+        menu_inicial()
+
+        opcion_inicial = input('Seleccione su opción [0-2]: ')
+        if opcion_inicial == '1':
+            registrar_usuario()
+        elif opcion_inicial == '2':
+            inicio_sesion = iniciar_sesion()
+            if inicio_sesion == True:
+                print('Iniciando Sistema...')
+        elif opcion_inicial == '3':
+            # ingreso_email = input('Ingrese Correo Electrónico: ')
+            # respuesta = validar_correo(ingreso_email)
+            # if respuesta == True:
+            #     print('Correo Válido.')
+            # else:
+            #     print('Correo Inválido.')
+            busqueda()
+        elif opcion_inicial == '0':
+            print('Saliendo...')
+            break
+        else:
+            print('Opción Incorrecta, vuelva a intentar...')
+
+
+app()
